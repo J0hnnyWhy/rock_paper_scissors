28a0741fcc7f0f16ae47dc25f5d311d8f06dac9c
John Young & Cleri Philiastides <student@epicodus.com>
2015-04-22 15:39:50 -0700

    Add and pass spec for scissors as the object and paper as the argument

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -8,9 +8,9 @@ describe('String#beats?') do
   it("returns true if paper is the object and rock is the argument") do
     expect("paper".beats?("rock")).to(eq(true))
   end
-  # it("returns true if scissors it the object and paper is the argument") do
-  #   expect("scissors".beats?("paper")).to(eq(true))
-  # end
+  it("returns true if scissors it the object and paper is the argument") do
+    expect("scissors".beats?("paper")).to(eq(true))
+  end
   it("returns false if the object and argument are the same") do
     expect("rock".beats?("rock")).to(eq("It's a tie"))
     expect("paper".beats?("paper")).to(eq("It's a tie"))
